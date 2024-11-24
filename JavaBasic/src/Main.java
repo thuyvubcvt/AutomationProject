d2894f2a2ee1e2763bbaaf0cab63a92517575c36
“thuyvubcvt” <vuthuy1661986@gmail.com>
2024-11-24 15:38:40 +0700

    add source code

---
+++ JavaBasic/src/Main.java
@@ -0,0 +1,24 @@
+import day1.DataTypesAndVariables;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        //khai bao class
+        DataTypesAndVariables dataTypesAndVariables=new DataTypesAndVariables();
+        // EX1
+        //dataTypesAndVariables.printHelloWorld();
+        //EX2
+        // dataTypesAndVariables.printString();
+        //EX3
+        // dataTypesAndVariables.printNumber();
+        //EX4
+        //dataTypesAndVariables.printBoolean();
+        //EX6
+        //dataTypesAndVariables.convertTemperature();
+        //Ex7
+        dataTypesAndVariables.convetInch();
+    }
+
+}
